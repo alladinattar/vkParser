2d24517e40e562a7b420fa077d9d9a288fd4fb00
Rinat <gitg>
2021-08-04 15:23:41 +0300

    get users and get followers ready

--- rabbitmq/consumer.go
+++ rabbitmq/consumer.go
@@ -1,32 +1,35 @@
 package rabbitmq
 
 import (
+	"encoding/json"
 	"github.com/streadway/amqp"
 	"log"
+	"net/http"
+	"os"
 )
 
 func GetTask() error {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
 		"vkTasks", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
@@ -39,7 +42,7 @@ func GetTask() error {
 		false,  // no-wait
 		nil,    // args
 	)
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 
@@ -47,11 +50,52 @@ func GetTask() error {
 
 	go func() {
 		for d := range msgs {
+
 			log.Printf("Received a message: %s", d.Body)
+			err := scrapeUser(string(d.Body))
+			if err != nil {
+				log.Println(err)
+			}
 		}
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
+	return nil
+}
+
+
+
+func scrapeUser(id string) error {
+	urlFriends := "https://api.vk.com/method/friends.get?user_id=" + id + "&v=5.52&access_token=" + os.Getenv("TOKEN")
+	friends, err := http.Get(urlFriends)
+	if err != nil {
+		return err
+	}
+	var friendsResponse struct {
+		Response struct {
+			Items []int `json:"items"`
+		} `json:"response"`
+	}
+	err = json.NewDecoder(friends.Body).Decode(&friendsResponse)
+	if err != nil {
+		return err
+	}
+
+	//выенсти в одну структуру
+	var followersResponse struct {
+		Response struct {
+			Items []int `json:"items"`
+		} `json:"response"`
+	}
+	urlFollowers := "https://api.vk.com/method/users.getFollowers?user_id=" + id + "&v=5.52&access_token=" + os.Getenv("TOKEN")
+	followers, err := http.Get(urlFollowers)
+	if err!=nil{
+		return err
+	}
+	err = json.NewDecoder(followers.Body).Decode(&followersResponse)
+	if err != nil {
+		return err
+	}
 	return nil
 }
